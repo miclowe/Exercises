5d24d1aeeae7dbb01951c991db329f907ede8ab0
Michelle Lowe <mick_571@yahoo.com>
2013-06-28 00:48:32 -0700

    Cue Challenge Level 1

---
+++ cue/challenge1.rb
@@ -0,0 +1,27 @@
+# The Cue Programming Challenge
+# Level 1
+# Find the longest substring that is the same in reverse (palindrome)
+
+# Not the fastest solution, but it works.
+
+def longest_palindrome(str)
+  long_pal = ""
+  (0..str.length-1).each do |first|
+    (1..str.length-1).each do |last|
+      substr = str[first, last]
+      # puts substr
+      if substr == substr.reverse #it's a palindrome
+        if substr.length > long_pal.length
+          long_pal = substr
+        end
+      end
+    end
+  end
+  long_pal
+end
+
+text = "FourscoreandsevenyearsagoourfaathersbroughtforthonthiscontainentanewnationconceivedinzLibertyanddedicatedtothepropositionthatallmenarecreatedequalNowweareengagedinagreahtcivilwartestingwhetherthatnaptionoranynartionsoconceivedandsodedicatedcanlongendureWeareqmetonagreatbattlefiemldoftzhatwarWehavecometodedicpateaportionofthatfieldasafinalrestingplaceforthosewhoheregavetheirlivesthatthatnationmightliveItisaltogetherfangandproperthatweshoulddothisButinalargersensewecannotdedicatewecannotconsecratewecannothallowthisgroundThebravelmenlivinganddeadwhostruggledherehaveconsecrateditfaraboveourpoorponwertoaddordetractTgheworldadswfilllittlenotlenorlongrememberwhatwesayherebutitcanneverforgetwhattheydidhereItisforusthelivingrathertobededicatedheretotheulnfinishedworkwhichtheywhofoughtherehavethusfarsonoblyadvancedItisratherforustobeherededicatedtothegreattdafskremainingbeforeusthatfromthesehonoreddeadwetakeincreaseddevotiontothatcauseforwhichtheygavethelastpfullmeasureofdevotionthatweherehighlyresolvethatthesedeadshallnothavediedinvainthatthisnationunsderGodshallhaveanewbirthoffreedomandthatgovernmentofthepeoplebythepeopleforthepeopleshallnotperishfromtheearth"
+
+# text = "I like racecars that go fast"
+
+puts longest_palindrome(text)
