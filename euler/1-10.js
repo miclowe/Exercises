7f7977e3f90ed80acafe600bf57b6d8fa57396b7
Michelle Lowe <mick_571@yahoo.com>
2013-07-01 23:44:02 -0700

    Renamed JS functions

--- euler/1-10.js
+++ euler/1-10.js
@@ -1,6 +1,6 @@
 // Problem 1
 // Find the sum of all the multiples of 3 or 5 below 1000.
-function multiples_of_3_and_5() {
+function multiplesOf3And5() {
   var total = 0;
   for (var i = 3; i < 1000; i += 1) {
     if (i%3 === 0 || i%5 === 0) {
@@ -9,12 +9,12 @@ function multiples_of_3_and_5() {
   }
   return total;
 }
-alert("The sum of all the multiples of 3 or 5 below 1000 is " + multiples_of_3_and_5() + ".");
+alert("The sum of all the multiples of 3 or 5 below 1000 is " + multiplesOf3And5() + ".");
 
 // Problem 2
 // Find the sum of the even values of the Fibonacci sequence below 4,000,000
 // 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, ...
-function sum_even_fibonacci(max) {
+function sumEvenFibonacci(max) {
   var total = 0;
   var num1 = 1;
   var num2 = 2;
@@ -29,5 +29,5 @@ function sum_even_fibonacci(max) {
   }
   return total;
 }
-alert("The sum of the even values of the Fibonacci sequence below 4,000,000 is " + sum_even_fibonacci(4000000) + ".");
+alert("The sum of the even values of the Fibonacci sequence below 4,000,000 is " + sumEvenFibonacci(4000000) + ".");
 
